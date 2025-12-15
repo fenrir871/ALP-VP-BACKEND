e84d54330f20eb38d6b46c8458f593871b9b9139
fenrir871 <wahyudibertrand02@gmail.com>
2025-12-15 13:13:36 +0700

    bb

---
+++ bin/models/TodayActivity-model.ts
@@ -0,0 +1,50 @@
+export interface TodayActivityPayload {
+    id: number
+    date: Date
+    steps: number
+    sleep_hours: number
+    calories: number
+    user_id: number
+}
+
+export interface CreateTodayActivityRequest {
+    date: Date | string
+    steps: number
+    sleep_hours: number
+    calories: number
+    user_id: number
+}
+
+export interface UpdateTodayActivityRequest {
+    date?: Date | string
+    steps?: number
+    sleep_hours?: number
+    calories?: number
+}
+
+export interface TodayActivityResponse {
+    id: number
+    date: Date
+    steps: number
+    sleep_hours: number
+    calories: number
+    user_id: number
+}
+
+export function toTodayActivityResponse(
+    id: number,
+    date: Date,
+    steps: number,
+    sleep_hours: number,
+    calories: number,
+    user_id: number
+): TodayActivityResponse {
+    return {
+        id: id,
+        date: date,
+        steps: steps,
+        sleep_hours: sleep_hours,
+        calories: calories,
+        user_id: user_id,
+    }
+}
---
+++ src/validations/dailyactivity-validations.ts
@@ -0,0 +1,45 @@
+import z, { ZodType } from "zod"
+
+export class DailyActivityValidation {
+  static readonly CREATE: ZodType = z.object({
+    date: z
+      .string({ error: "Date must be a string (ISO)!" })
+      .min(1, { error: "Date cannot be empty!" }),
+    steps: z
+      .number({ error: "Steps must be a number!" })
+      .int({ error: "Steps must be an integer!" })
+      .nonnegative({ error: "Steps cannot be negative!" }),
+    sleep_hours: z
+      .number({ error: "Sleep hours must be a number!" })
+      .nonnegative({ error: "Sleep hours cannot be negative!" }),
+    calories: z
+      .number({ error: "Calories must be a number!" })
+      .int({ error: "Calories must be an integer!" })
+      .nonnegative({ error: "Calories cannot be negative!" }),
+    user_id: z
+      .number({ error: "User ID must be a number!" })
+      .int({ error: "User ID must be an integer!" })
+      .positive({ error: "User ID must be a positive integer!" }),
+  })
+
+  static readonly UPDATE: ZodType = z.object({
+    date: z
+      .string({ error: "Date must be a string (ISO)!" })
+      .min(1, { error: "Date cannot be empty!" })
+      .optional(),
+    steps: z
+      .number({ error: "Steps must be a number!" })
+      .int({ error: "Steps must be an integer!" })
+      .nonnegative({ error: "Steps cannot be negative!" })
+      .optional(),
+    sleep_hours: z
+      .number({ error: "Sleep hours must be a number!" })
+      .nonnegative({ error: "Sleep hours cannot be negative!" })
+      .optional(),
+    calories: z
+      .number({ error: "Calories must be a number!" })
+      .int({ error: "Calories must be an integer!" })
+      .nonnegative({ error: "Calories cannot be negative!" })
+      .optional(),
+  })
+}
